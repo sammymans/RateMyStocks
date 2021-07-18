d43202f8112e387b592cdc28f53a5ad6ffe82fff
sammymans <sfarnum3@uwo.ca>
2021-07-18 13:32:47 -0400

    first part of solution to Jason

--- problems/solution2.py
+++ problems/solution2.py
@@ -3,17 +3,58 @@ import pandas_datareader.data as web
 import datetime as dt
 
 def main():
+    #-----------------------------------
+    #1 - Get basic stock pricing on day-to-day basis
+    #-----------------------------------
+    
     startdate = dt.datetime(2020,1,1)
     enddate = dt.datetime(2021,1,1)
 
-    #result = web.DataReader('TSLA', 'yahoo', startdate, enddate)
+    df = web.DataReader('TSLA', 'yahoo', startdate, enddate)
 
-    # Save to CSV
-    #result.to_csv('tesla.csv')
+    # Print first couple data entries to make sure data is correct
+    print(df.head())
+
+    # 1a - Save to CSV
+    df.to_csv('tesla.csv')
 
     # Read from CSV
-    #result = pd.read_csv('tesla.csv')
-    #print(result)
+    df = pd.read_csv('tesla.csv')
+    print(df)
+
+    # 1b - Find Average
+    
+    #Print average for 'High' column
+    print(df["High"].mean())
+    #Print average for 'Low' column using dot notation
+    print(df.Low.mean())
+    #Print mean of multiple columns
+    print(df[["Open", "Close"]].mean())
+    #General description of dataframe
+    print(df.describe())
+
+    #-----------------------------------
+    #2
+    # Jason invested $12,000 worth of TSLA at the beginning of 2020.
+    # How much is his investment worth now?
+    #-----------------------------------
+
+    initial = 12000
+
+    openCol = df["Open"]
+    openPrice = openCol[0]
+
+    closeCol = df["Close"]
+    closePrice = closeCol[len(closeCol)-1]
+
+    gain = closePrice - openPrice
+    percentGain = gain / openPrice
+
+    print(gain, percentGain)
+
+    newAmt = initial * percentGain
+
+    print(newAmt)
 
 if __name__ == "__main__":
     main()
