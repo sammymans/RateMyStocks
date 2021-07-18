8d8f8efb53cebee2aaa7970f267ca349ef3f18f2
sammymans <sfarnum3@uwo.ca>
2021-07-18 14:48:34 -0400

    Tammy soln added

--- problems/solution2.py
+++ problems/solution2.py
@@ -1,3 +1,4 @@
+from os import close
 import pandas as pd
 import pandas_datareader.data as web
 import datetime as dt
@@ -16,7 +17,7 @@ def main():
     print(df.head())
 
     # 1a - Save to CSV
-    df.to_csv('tesla.csv')
+    #df.to_csv('tesla.csv')
 
     # Read from CSV
     df = pd.read_csv('tesla.csv')
@@ -34,27 +35,71 @@ def main():
     print(df.describe())
 
     #-----------------------------------
-    #2
+    # 2
     # Jason invested $12,000 worth of TSLA at the beginning of 2020.
-    # How much is his investment worth now?
+    # How much is his investment worth at the end of 2020?
     #-----------------------------------
 
-    initial = 12000
+    initialJ = 12000
 
     openCol = df["Open"]
     openPrice = openCol[0]
 
-    closeCol = df["Close"]
+    closeCol = df["Adj Close"]
     closePrice = closeCol[len(closeCol)-1]
 
     gain = closePrice - openPrice
     percentGain = gain / openPrice
 
-    print(gain, percentGain)
+    #print(gain, percentGain)
 
-    newAmt = initial * percentGain
+    newAmt = initialJ * percentGain
 
-    print(newAmt)
+    print("Jason's initial investment grew to ${:,.2f}".format(newAmt))
+
+    #-----------------------------------
+    # 3
+    # Tammy's circumstance:
+    # - She has $12,000 worth of money to be invested
+    # - Only invests $1,000 if and only if the stock drops 5% from previous day
+    # How much is her investment worth at the end of 2020?
+    #-----------------------------------
+
+    savingsTammy = 12000
+    updateSavings = 0
+
+    invAmt = 1000
+    tslaValue = []
+    resultTammy = 0
+
+    lastDayVal = closeCol[len(closeCol)-1]
+
+    # Recall openCol and closeCol from #2
+
+    for i in range(1,len(openCol)):
+
+        if((openCol[i] - closeCol[i-1])/closeCol[i-1] <= -0.05):
+
+            tslaValue.append(openCol[i])
+
+            savingsTammy = savingsTammy - invAmt
+            updateSavings += invAmt
+            if(savingsTammy == 0):
+                break
+
+    for val in tslaValue:
+        resultTammy += (lastDayVal - val)*1000/val
+
+    resultTammy += updateSavings
+    
+    print("Tammy's initial investment grew to ${:,.2f}".format(resultTammy))
+
+    
+    
+
+
+
+
 
 if __name__ == "__main__":
     main()
