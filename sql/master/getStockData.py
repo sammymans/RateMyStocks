3400a4a1399bba7d2764dc7e85ee1b154e628034
sammymans <sfarnum3@uwo.ca>
2021-08-16 23:00:09 -0400

    finished

--- sql/master/getStockData.py
+++ sql/master/getStockData.py
@@ -1,10 +1,15 @@
-
+from os import name
+from pandas.core.frame import DataFrame
 import pandas_datareader.data as web
 import datetime
 
 import pandas as pd
 
 import mysql.connector
+
+import csv
+
+from sqlalchemy import create_engine
 
 # First, we need to read all of the ticker symbols from the excel that I created
 symbol = []
@@ -32,24 +37,24 @@ file_out = 'yahoo_prices_volumes_for_RateMyStonks.csv'
 # Loop through tickers in symbol list
 # If the ticker cannot be found in yahoo finance, just skip over it
 
-i=0
-while i<len(symbol):
-    try:
-        df = web.DataReader(symbol[i], 'yahoo', startdate, enddate)
-        df.insert(0,'Symbol',symbol[i])
-        df = df.drop(['Adj Close'], axis=1)
-        if i == 0:
-            df.to_csv(path_out+file_out)
-            print (i, symbol[i],'has data stored to csv file')
-            i=i+1
-        else:
-            df.to_csv(path_out+file_out,mode = 'a',header=False)
-            print (i, symbol[i],'has data stored to csv file')
-            i=i+1
-    except:
-        print("No information for symbol or file is open in Excel:")
-        print (i,symbol[i])
-        i=i+1
+# i=0
+# while i<len(symbol):
+#     try:
+#         df = web.DataReader(symbol[i], 'yahoo', startdate, enddate)
+#         df.insert(0,'Symbol',symbol[i])
+#         df = df.drop(['Adj Close'], axis=1)
+#         if i == 0:
+#             df.to_csv(path_out+file_out)
+#             print (i, symbol[i],'has data stored to csv file')
+#             i=i+1
+#         else:
+#             df.to_csv(path_out+file_out,mode = 'a',header=False)
+#             print (i, symbol[i],'has data stored to csv file')
+#             i=i+1
+#     except:
+#         print("No information for symbol or file is open in Excel:")
+#         print (i,symbol[i])
+#         i=i+1
 
 # Now that we have our csv file filled with all of the data we need...    
 # Lets's Make a connection to the SQL Server
@@ -71,7 +76,41 @@ cursor = connection.cursor()
 data = pd.read_csv(path_out + file_out)
 #print(data)
 
+# Change csv into pandas dataframe
+df = DataFrame(data, columns=['Date','Symbol','High','Low','Open','Close','Volume'])
+#print(df)
+
 # Make a table in SQL where we will store our data from the csv
+# cursor.execute("""CREATE TABLE yahoo_prices_for_stocks (
+#     Date date,
+#     Symbol nvarchar(5),
+#     High decimal(15,2),
+#     Low decimal(15,2),
+#     Open decimal(15,2),
+#     Close decimal(15,2),
+#     Volume int
+# )"""
+# )
+
+#-----------------------------------------------------
+# At this stage, we have a csv created with all of the stock information
+# With the csv file, we have also made a pandas dataframe with all of the same information
+# We have also created an SQL table, that is in a database initialized in SQL Workbench
+# All that is left is too finally add the data, either from the csv or pandas dataframe, to the SQL table
+#-----------------------------------------------------
+
+
+# Finally, we need to insert rows from our csv file into our SQL Table
+
+for i, row in df.iterrows():
+    sql = """ INSERT INTO yahoo_prices_for_stocks (Date, Symbol, High, Low, Open, Close, Volume)
+    VALUES (%s,%s,%s,%s,%s,%s,%s);
+    """
+    cursor.execute(sql, tuple(row))
+    connection.commit()
+
+
+
 
 
 
